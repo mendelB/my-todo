651b3e4ddecf5418ebfad5a722ae577cfade579b
mendelB <mendelblesofsky@gmail.com>
2016-11-30 19:18:37 -0500

    add basic create display functioanlity for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,17 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
+    @task = current_user.tasks.build
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,8 +24,15 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def index
+    @tasks = Task.all
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title)
   end
 end
