fce214afa5cd6b5b449999942dfb1d2009bcf4dc
mendelB <mendelblesofsky@gmail.com>
2016-12-10 20:51:48 -0500

    add user_tasks controller some crud

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,2 +1,18 @@
 class UserTasksController < ApplicationController
+
+  def create
+    current_user.user_tasks.create(user_task_params)
+    redirect_to root_path
+  end
+
+  def update
+    task = UserTask.find(params[:id])
+    task.update(complete: !task.complete)
+    head :ok
+  end
+
+  private
+  def user_task_params
+    params.require(:user_task).permit(:task_id)
+  end
 end
